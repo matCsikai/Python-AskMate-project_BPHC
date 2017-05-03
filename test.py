1c2f3ab6f392dab0b1635430619c03e38951fea4
matCsikai <ccsmatyas@gmail.com>
2017-05-03 21:54:29 +0200

    probably working question+answes management function

--- test.py
+++ test.py
@@ -8,14 +8,20 @@ app = Flask(__name__)
 def all_answers(question_id):
     answer_database = common.read_data('answer.csv')
     question_database = common.read_data('question.csv')
+
+    answers = []
+    for data_line in answer_database:
+        if str(question_id) in data_line[3]:
+            answers.append(data_line)
+    return render_template('all_answers.html', answers=answers)
+
     for data_line in question_database:
-        if question_id == data_line[0]:
+        if str(question_id) in data_line[0]:
             question_line = data_line
-            print(question_line)
-            
+            return render_template('all_answers.html', question_line=question_line)
 
     
-    return render_template ('all_answers.html', question_line=question_line)
+
 
 
 
