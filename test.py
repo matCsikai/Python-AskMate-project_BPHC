7bcb66d723cd74af6dc3310652b7e1aea43a4cc8
matCsikai <ccsmatyas@gmail.com>
2017-05-04 11:23:22 +0200

    working all_answers func

--- test.py
+++ test.py
@@ -8,17 +8,17 @@ app = Flask(__name__)
 def all_answers(question_id):
     answer_database = common.read_data('answer.csv')
     question_database = common.read_data('question.csv')
-
     answers = []
     for data_line in answer_database:
         if str(question_id) in data_line[3]:
             answers.append(data_line)
-    return render_template('all_answers.html', answers=answers)
+    print(answers)
 
     for data_line in question_database:
         if str(question_id) in data_line[0]:
             question_line = data_line
-            return render_template('all_answers.html', question_line=question_line)
+            print(question_line)
+    return render_template('all_answers.html', question_line=question_line)
 
     
 
